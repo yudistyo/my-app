cf01bf68e1cf1490935ba64f3c68be6ba69aa16b
yudistyo <samudrahati45@gmail.com>
2020-04-04 13:15:35 +0700

    commit initial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React,{Component,useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [Input,setInput]=useState('');
+  const [count, setCount] = useState(0);  
+
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <input type='text' value={Input} onChange={event => setInput(event.target.value)} placeholder='User name' style={{marginTop:20}}></input>
+       <input type='password' placeholder='Password' style={{marginTop:20}}></input>
+       <button style={{marginTop:20}} onClick={()=>{alert('ok')}}>Login</button>
+       
+       <p>{Input}</p>
+        <a style={{marginTop:20}}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -19,6 +25,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
     </div>
   );
 }
